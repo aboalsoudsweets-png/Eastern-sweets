cc8a97ea15c4300635483170f2ca9a1c557c33ad
aboalsoudsweets-png <aboalsoudsweets@gmail.com>
2026-04-09 16:15:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -821,7 +821,7 @@ updateWeightPrice(state.selectedDrink, multiplier);
 // ========== HANDLE QUICK ADD ==========
 function handleQuickAdd(event, drinkId) {
 
-const closedItems = ["", "", "","","","","",""];
+const closedItems = ["9000", "2", "40055","","","","",""];
 
 if (closedItems.includes(drinkId)) {
   showToast("❌ الصنف غير متوفر حالياً");
